37f724c6d752e647899dbf15d47ae72d3ce639ac
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-31 15:52:34 +0400

    Javaascript Input Slider Done

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const togglebutton = document.querySelector('.togglebg');
+const getPrice = document.getElementById('price');
+const pageView = document.getElementById('pageview');
+const slider = document.querySelector('.slider');
+slider.addEventListener('input', function() {
+  const sliderValue = parseInt(slider.value);
+  if (sliderValue === 1) {
+    getPrice.textContent = '$18.00';
+    pageView.textContent = '20K PAGEVIEWS';
+    console.log(pageView.textContent);
+  } else if (sliderValue === 2) {
+   getPrice.textContent = '$36.00';
+   pageView.textContent = '40K PAGEVIEWS';
+  } else if (sliderValue === 3) {
+   getPrice.textContent = '$54.00';
+   pageView.textContent = '60K PAGEVIEWS';
+  } else if (sliderValue === 4) {
+   getPrice.textContent = '$72.00';
+   pageView.textContent = '80K PAGEVIEWS';
+  } else if (sliderValue === 5) {
+   getPrice.textContent = '$90.00';
+   pageView.textContent = '100K PAGEVIEWS';
+  }
+});
 
-
-togglebutton.addEventListener('click', () => {
-    if (togglebutton.classList.contains('active')) {
-        togglebutton.classList.remove('active');
-    } else {
-        togglebutton.classList.add('active');
-    }
+togglebutton.addEventListener('click', function() {
+  togglebutton.classList.toggle('active');
+  if (togglebutton.classList.contains('active')) {
+    getPrice.textContent = '39'
+  } else {
+    slider.classList.remove('active');
+    getPrice.textContent = "";
+  }
 });
