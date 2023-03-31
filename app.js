fabe8a276d936c9752c36386772f5206771e721e
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-31 14:44:24 +0400

    Mobile Version Done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const togglebutton = document.querySelector('.togglebg');
 
-//add click on togglebutton 
+
 togglebutton.addEventListener('click', () => {
-    //toggle button is clicked
     if (togglebutton.classList.contains('active')) {
         togglebutton.classList.remove('active');
     } else {
