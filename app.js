a120da8944d333e94c9eb7d3cb7e47db679c467d
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-31 16:14:04 +0400

    Javascript Logic Done

--- app.js
+++ app.js
@@ -2,33 +2,55 @@ const togglebutton = document.querySelector('.togglebg');
 const getPrice = document.getElementById('price');
 const pageView = document.getElementById('pageview');
 const slider = document.querySelector('.slider');
+let originalPrice = "";
+
 slider.addEventListener('input', function() {
   const sliderValue = parseInt(slider.value);
   if (sliderValue === 1) {
     getPrice.textContent = '$18.00';
     pageView.textContent = '20K PAGEVIEWS';
-    console.log(pageView.textContent);
   } else if (sliderValue === 2) {
-   getPrice.textContent = '$36.00';
-   pageView.textContent = '40K PAGEVIEWS';
+    getPrice.textContent = '$36.00';
+    pageView.textContent = '40K PAGEVIEWS';
   } else if (sliderValue === 3) {
-   getPrice.textContent = '$54.00';
-   pageView.textContent = '60K PAGEVIEWS';
+    getPrice.textContent = '$54.00';
+    pageView.textContent = '60K PAGEVIEWS';
   } else if (sliderValue === 4) {
-   getPrice.textContent = '$72.00';
-   pageView.textContent = '80K PAGEVIEWS';
+    getPrice.textContent = '$72.00';
+    pageView.textContent = '80K PAGEVIEWS';
   } else if (sliderValue === 5) {
-   getPrice.textContent = '$90.00';
-   pageView.textContent = '100K PAGEVIEWS';
+    getPrice.textContent = '$90.00';
+    pageView.textContent = '100K PAGEVIEWS';
   }
+  originalPrice = getPrice.textContent;
 });
 
 togglebutton.addEventListener('click', function() {
-  togglebutton.classList.toggle('active');
-  if (togglebutton.classList.contains('active')) {
-    getPrice.textContent = '39'
-  } else {
-    slider.classList.remove('active');
-    getPrice.textContent = "";
-  }
-});
+   togglebutton.classList.toggle('active');
+ 
+   let price = 0;
+   const sliderValue = parseInt(slider.value);
+   switch (sliderValue) {
+     case 1:
+       price = 18;
+       break;
+     case 2:
+       price = 36;
+       break;
+     case 3:
+       price = 54;
+       break;
+     case 4:
+       price = 72;
+       break;
+     case 5:
+       price = 90;
+       break;
+   }
+ 
+   if (togglebutton.classList.contains('active')) {
+     getPrice.textContent = '$' + (price * 12 * 0.75).toFixed(2);
+   } else {
+     getPrice.textContent = '$' + price.toFixed(2);
+   }
+ });
